3464a187640a38e0085c70ac43a6e04b886919d4
oscarsidsa81 <ovilches@sid-sa.com>
2026-04-09 15:41:14 +0200

    Dejamos en este commit sólo los campos y vistas relacionados con los stocks pronosticados desde sale.order.line y sus vistas, no teniendo dependencias adicionales más allá de "sale", "sale_stock", "stock"

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     "name": "sid_sale_line_stock",
     "version": "15.0.1.0.0",
     "category": "Sales",
-    "summary": "Campos de stock pronosticado por almacÃ©n (Madrid/Puertollano) en sale.order.line.",
+    "summary": "Campos de stock pronosticado por almacén (Madrid/Puertollano) en sale.order.line.",
     "author": "oscarsidsa81",
     "license": "LGPL-3",
     "depends": ["sale", "sale_stock", "stock"],
